b88051e298a1951990b4ecccc12d6140cf8be023
danialbleile <danial.bleile@wsu.edu>
2014-12-08 16:15:47 -0800

    Revert "Revert "Clean up old code""
    This reverts commit 15af46fb1a400891696422f7880d187d9d29cb10.

--- inc/inc-background-banner.php
+++
@@ -1,3 +0,0 @@
-<?php if( isset( $anchor ) ) echo '<a name="' . $anchor . '"></a>';?>
-<div class="pagebuilder-row-background unbound recto verso" style="width: 1545px; margin-right: -79.5px; margin-left: -277.5px; background-image: url(<?php echo $image; ?>);">
-</div>
